4a73c3dc99fb39d169acaf5cf433f63804654136
Ben Sedat <bsedat@tinfoilsecurity.com>
2013-02-21 11:18:53 -0800

    Saving a Transition now performs the transition on the associated issue

--- lib/jira/resource/transition.rb
+++ lib/jira/resource/transition.rb
@@ -9,6 +9,17 @@ module JIRA
 
       has_one :issue, :class => JIRA::Resource::Issue, :attribute_key => 'issue'
       has_one :status, :class => JIRA::Resource::Status, :attribute_key => 'to'
+
+      def save!(attrs={})
+        attrs.merge!( :transition => { :id => id } )
+        client.send( :post, "#{issue.url}/transitions", attrs.to_json )
+        @expanded = false
+        true
+      end
+
+      def save(attrs={})
+        super
+      end
     end
   end
 end
